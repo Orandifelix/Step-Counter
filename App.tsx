b324cad52e86b0bc622581ee39444b8d765c3f24
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-12-06 04:59:12 +0300

    initial commint

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+type ValueProps ={
+ label: string;
+ value: string;
+}
+
+const Value = ({ label, value }:ValueProps) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Stepcounter Counters from Orandi</Text>
+      <View style={{ flexDirection: "row" }}>
+        <Value label="Steps" value="1234" />
+        <Value label="Distance" value="1.234KM" />
+      </View>
+      <Value label="Flights Climbed" value="12" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    padding: 20,
+    justifyContent: "center",
   },
+
+  value: {
+    fontSize: 35,
+    color: '#AFB3BE',
+    fontWeight:  '500',
+
+
+  },
+
+  label: {
+    color: 'white',
+    fontSize: 20,
+
+  },
+  valueContainer:{
+    marginRight: 50,
+    marginVertical: 10,
+
+  }
 });
